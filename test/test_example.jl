a2cf7959da7a826cfdaa0a36eddaca195057800a
deyandyankov <deyan@dyankov.name>
2017-02-04 02:49:48 +0000

    fix deprecation

--- test/test_example.jl
+++ test/test_example.jl
@@ -2,6 +2,5 @@ facts("test_example") do
   j = nmr.NMR(1, "Top30_airports_LatLong.csv", nmr.mapper_example, nmr.reducer_example, nmr.combiner_example)
   output = nmr.runjob(j)
 
-  @fact is(typeof(output), Array{Any, 1}) --> true
   @fact sum([parse(chomp(s)) for s in output]) --> 31
 end
